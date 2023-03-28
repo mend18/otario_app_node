e8b86d352146d7b8df88e27904d10c263dc4476d
Pedro <mendonca.pedro.18@gmail.com>
2023-03-28 10:25:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# banco_otario_node
+pequeno website em node.js para testes de azure app service
---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+
+var app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}!');
+});
